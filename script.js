b7d632bb384b72af037870025848c1900c2cbeef
JLA-STD-082 <svass@jlastudents.org>
2025-03-02 23:07:36 -0500

    Update

--- script.js
+++ script.js
@@ -202,18 +202,18 @@ function filterShivrian() {
         const paratypings = shivrian.paratypings ? shivrian.paratypings.toLowerCase() : "";
         const isMatch = (name.includes(lastQuery) || lastQuery === id) && !category.includes("halloween") && !category.includes("frostbite") && (!category.includes("alt") || Alt) && !category.includes("npc") && !category.includes("npcalt");
         const isAltMatch = (name.includes(lastQuery) || lastQuery === id) && (category.includes("alt"));
-        const isTalHelp = credits.includes("tal+") && category==("") ;
-        const isTaltHelp = credits.includes("tal+") && category!=("") ;
-        const isJakeHelp = credits.includes("jake+") && category==("") ;
-        const isJaltHelp = credits.includes("jake+") && category!=("") ;
-        const isShaneHelp = credits.includes("shane+") && category==("") ;
-        const isShaltHelp = credits.includes("shane+") && category!=("") ;
-        const isArielHelp = credits.includes("ariel+") && category==("") ;
-        const isArialtHelp = credits.includes("ariel+") && category!=("") ;
-        const isIvriHelp = credits.includes("ivri+") && category==("") ;
-        const isIvraltHelp = credits.includes("ivri+") && category!=("") ;
-        const isAmoHelp = credits.includes("amo+") && category==("") ;
-        const isAmaltHelp = credits.includes("amo+") && category!=("") ;
+        const isTalHelp = credits.includes("tal+") && (category==("") || category.includes("halloween") || category.includes("frostbite")) && category.includes("halloweenforme");
+        const isTaltHelp = credits.includes("tal+") && (category==("") || category.includes("halloweenforme") || category.includes("frozen"));
+        const isJakeHelp = credits.includes("jake+") && (category==("") || category.includes("halloween") || category.includes("frostbite")) && category.includes("halloweenforme");
+        const isJaltHelp = credits.includes("jake+") && (category==("") || category.includes("halloweenforme") || category.includes("frozen"));
+        const isShaneHelp = credits.includes("shane+") && (category==("") || category.includes("halloween") || category.includes("frostbite")) && category.includes("halloweenforme");
+        const isShaltHelp = credits.includes("shane+") && (category==("") || category.includes("halloweenforme") || category.includes("frozen"));
+        const isArielHelp = credits.includes("ariel+") && (category==("") || category.includes("halloween") || category.includes("frostbite")) && category.includes("halloweenforme");
+        const isArialtHelp = credits.includes("ariel+") && (category==("") || category.includes("halloweenforme") || category.includes("frozen"));
+        const isIvriHelp = credits.includes("ivri+") && (category==("") || category.includes("halloween") || category.includes("frostbite")) && category.includes("halloweenforme");
+        const isIvraltHelp = credits.includes("ivri+") && (category==("") || category.includes("halloweenforme") || category.includes("frozen"));
+        const isAmoHelp = credits.includes("amo+") && (category==("") || category.includes("halloween") || category.includes("frostbite")) && category.includes("halloweenforme");
+        const isAmaltHelp = credits.includes("amo+") && (category==("") || category.includes("halloweenforme") || category.includes("frozen"));
 
         const isCredit = credits.includes("+");
 
