af40f1632a68320230abdff6d47716a7ea233239
Savana-Unana <svass@jlastudents.org>
2025-11-17 14:20:29 -0500

    ts

--- JSs/script.js
+++ JSs/script.js
@@ -45,7 +45,7 @@ function updateButtons() {
         { text: "Type Chart", href: "typechart.html", bg: "linear-gradient(135deg, #e48dff, #c300ff)" },
         { text: "Chat", href: "IconTest/index.html", bg: "linear-gradient(135deg, #eaa78f, #c96e4e)" },
         { text: "Moves/Abilities", href: "construction.html", bg: "linear-gradient(135deg, #ffbc62, #ff9200)" },
-        { text: "Credits", href: "construction.html", bg: "linear-gradient(135deg, #ffd0d0, #fd9595)" }
+        { text: "Credits", href: "friend.html", bg: "linear-gradient(135deg, #ffd0d0, #fd9595)" }
       ]
     : [
         { text: "Catalog", href: "catalog.html", bg: "linear-gradient(135deg, #a3e635, #65a30d)" },
